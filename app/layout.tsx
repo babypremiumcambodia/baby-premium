f1475a2a634266336176a06f1c06b9e476706650
Menghourt Sok <menghourtsok@Menghourts-MacBook-Air.local>
2026-07-07 13:25:11 +0700

    Add Khmer font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import "@fontsource/kantumruy-pro/400.css";
+import "@fontsource/kantumruy-pro/500.css";
+import "@fontsource/kantumruy-pro/600.css";
+import "@fontsource/kantumruy-pro/700.css";
 import "./globals.css";
 import Script from "next/script";
 
@@ -25,17 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="km"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
       <body>
-  <Script
-    src="https://telegram.org/js/telegram-web-app.js"
-    strategy="beforeInteractive"
-  />
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
 
-  {children}
-</body>
+        {children}
+      </body>
     </html>
   );
 }
