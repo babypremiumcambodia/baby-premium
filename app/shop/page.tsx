02817c4b33476e8484522edc5cf7cb4a3a53bbed
Menghourt Sok <menghourtsok@Menghourts-MacBook-Air.local>
2026-07-27 18:12:50 +0700

    Fix shop suspense

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import ShopClient from "@/components/shop/ShopClient";
 import { getProducts } from "@/lib/products";
-
 
 export default async function ShopPage() {
   const products = await getProducts();
 
-  return <ShopClient products={products} />;
+  return (
+    <Suspense fallback={<div className="p-6">Loading shop...</div>}>
+      <ShopClient products={products} />
+    </Suspense>
+  );
 }
